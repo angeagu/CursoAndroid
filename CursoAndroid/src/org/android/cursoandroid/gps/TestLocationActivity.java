1f37a857a4227137208fe47b15d653115b53ea72
angeagu <angeagu@gmail.com>
2013-10-16 12:28:27 +0200

    Añadido módulo tutorial de la utilización de GPS con Android nativo.

---
+++ CursoAndroid/src/org/android/cursoandroid/gps/TestLocationActivity.java
@@ -0,0 +1,271 @@
+package org.android.cursoandroid.gps;
+
+import java.text.DecimalFormat;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.android.cursoandroid.R;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class TestLocationActivity extends Activity {
+	    /** Called when the activity is first created. */
+	    
+	    private Map mapaCoordenadas = new HashMap();
+	    private Date fechaInicio;
+	    private Date fechaFin;
+	    
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        
+	        setContentView(R.layout.location);
+	        
+	        TextView textViewLocation = (TextView)this.findViewById(R.id.textViewLocation);
+	        textViewLocation.setTextSize(18);
+	        textViewLocation.setTextColor(Color.RED);
+	        textViewLocation.setBackgroundColor(Color.BLACK);
+	        
+	        final LocationManager locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+	        //Obtenemos el provider del GPS.
+	        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+	            
+	        	//Si el GPS está encendido...	
+	        	LocationProvider provider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+	        	String name = provider.getName();
+	        	int accuracy = provider.getAccuracy();
+	        	int powerRequirement = provider.getPowerRequirement();
+	        
+	        	//Ojo, con el método getAllProviders obtenemos todos los providers,
+	        	//por ejemplo el GPS, el Network, etc.
+	        
+	        	//Con la clase Criteria podríamos definir criterios para elegir el
+	        	//mejor provider con ciertas características:
+	        
+	        	/*
+	        	 * Este objeto Criteria indica que queremos un Provider con precisión
+	        	 * alta y que pueda proporcionar la altitud.
+	        	 * 
+	        	Criteria req = new Criteria();
+	        	req.setAccuracy(Criteria.ACCURACY_FINE);
+	        	req.setAltitudeRequired(true);
+	        	locationManager.getBestProvider(criteria, false);
+	        	 */
+	        	final LocationListener listener = new LocationListener() {
+	        		public void onLocationChanged(Location location) {
+	        			// TODO Auto-generated method stub
+	        			if (fechaInicio==null)
+	        				fechaInicio = new Date();
+	        			
+	        			if (location!=null) {
+	        				
+	        				float precision = location.getAccuracy();
+	        				double altitud = location.getAltitude();
+	        				double latitud = location.getLatitude();
+	        				double longitud = location.getLongitude();
+	        				double velocidad = location.getSpeed();
+	        				velocidad = velocidad * (3600/1000);
+	        				long tiempo = location.getTime();
+	        				Date time = new Date(tiempo);
+	        				
+	        				actualizarInterfaz(precision,altitud,latitud,longitud,velocidad,time);
+	        			}
+	        		}
+
+	        		public void onProviderDisabled(String provider) {
+	        			// TODO Auto-generated method stub
+	        			mostrarMensaje("GPS desactivado.");	
+	        		}
+
+	        		public void onProviderEnabled(String provider) {
+	        			// TODO Auto-generated method stub
+	        			mostrarMensaje("GPS sactivado.");
+	        		}
+
+	        		public void onStatusChanged(String provider, int status, Bundle extras) {
+	        			// TODO Auto-generated method stub
+	        			switch (status) {
+	        				case 0: mostrarMensaje("GPS FUERA DE SERVICIO"); break; 
+	        				case 1: mostrarMensaje("GPS TEMPORALMENTE NO DISPONIBLE"); break;
+	        				case 2: mostrarMensaje("GPS DISPONIBLE"); break;
+	        			}
+	        		}
+	        		
+	        	};
+	        	
+	        	//Definimos el listener del Botón Obtener Datos GPS
+	        	Button botonObtenerDatos = (Button) this.findViewById(R.id.botonObtenerDatosGps);
+	        	botonObtenerDatos.setOnClickListener(new OnClickListener() {
+	        		public void onClick(View v) {		       			
+	        			/*
+	        			Intent i = new Intent(TestLocationActivity.this,TestLocationActivity.class);
+	        			//startActivityForResult(i, 1);
+	        			startActivity(i);
+	        			*/
+	        			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+	    	        			1000, 0, listener);
+	        			v.setEnabled(false);
+	        			Button botonDetener = (Button) findViewById(R.id.botonDetener);
+	        			botonDetener.setEnabled(true);
+	        			mapaCoordenadas.clear();
+	        		}
+	        	});
+	        	
+	        	//Definimos el listener del Botón Detener
+	        	Button botonDetener = (Button) this.findViewById(R.id.botonDetener);
+	        	botonDetener.setOnClickListener(new OnClickListener() {
+	        		public void onClick(View v) {   			
+	        			//Detenemos la obtención de datos del GPS.
+	        			locationManager.removeUpdates(listener);
+	        			v.setEnabled(false);
+	        			Button botonObtenerDatos = (Button) findViewById(R.id.botonObtenerDatosGps);
+	        			botonObtenerDatos.setEnabled(true);
+	        			fechaFin = new Date();
+	        		}
+	        	});
+	        	
+	        	//Definimos el listener del Botón Detener
+	        	Button botonCalcularDistancia = (Button) this.findViewById(R.id.botonCalcularDistancia);
+	        	botonCalcularDistancia.setOnClickListener(new OnClickListener() {
+	        		public void onClick(View v) {   			
+	        			//Detenemos la obtención de datos del GPS.
+	        			locationManager.removeUpdates(listener);
+	        			calcularInforme(mapaCoordenadas);
+	        		}
+	        	});
+	        	
+	        	
+	        }
+	        else {
+	        	//Si el GPS no está encendido...
+	        	VentanaAlerta.mostrarAlerta(this, "El GPS no está activado.");
+	        }
+	        
+	        
+	    }
+	  
+	private void actualizarInterfaz (float precision,double altitud,
+			double latitud,double longitud,double velocidad,Date time) {
+		
+		
+		EditText etLatitud = (EditText) this.findViewById(R.id.EditTextLatitud);
+		etLatitud.setText(Double.toString(latitud));
+		EditText etLongitud = (EditText) this.findViewById(R.id.EditTextLongitud);
+		etLongitud.setText(Double.toString(longitud));
+		mapaCoordenadas.put(mapaCoordenadas.size(), String.valueOf(latitud)+"%"+String.valueOf(longitud));
+		
+		EditText etVelocidad = (EditText) this.findViewById(R.id.EditTextVelocidad);
+		etVelocidad.setText(Double.toString(velocidad));
+		EditText etTiempo = (EditText) this.findViewById(R.id.EditTextTiempo);
+		etTiempo.setText((time.toGMTString()));
+		EditText etPrecision = (EditText) this.findViewById(R.id.EditTextPrecision);
+		etPrecision.setText(Float.toString(precision));
+		EditText etAltitud = (EditText) this.findViewById(R.id.EditTextAltitud);
+		etAltitud.setText(Double.toString(altitud));
+		
+	}
+	
+	private void mostrarMensaje(String mensaje) {
+		VentanaAlerta.mostrarAlerta(this, mensaje);
+	}
+	
+	private void calcularInforme(Map mapaCoordenadas) {
+		double distancia = 0;
+		double latitud1;
+		double longitud1;
+		double latitud2;
+		double longitud2;
+		
+		//Primero el tiempo empleado.
+		Calendar c1 = Calendar.getInstance();
+		Calendar c2 = Calendar.getInstance();
+		c1.setTime(fechaInicio);
+		c2.setTime(fechaFin);
+		long segundosTotales = cantidadTotalSegundos(c1,c2);
+		long horas = segundosTotales/3600;  
+        long minutos = (segundosTotales-(3600*horas))/60;  
+        long segundos = segundosTotales-((horas*3600)+(minutos*60)); 
+		
+		if (mapaCoordenadas.isEmpty()) {
+			VentanaAlerta.mostrarAlerta(this, "No hay coordenadas GPS disponibles.");
+		}
+		else {
+			Collection col = mapaCoordenadas.values();
+			Iterator it = col.iterator();
+			while (it.hasNext()) {
+				//Primera coordenada
+				String cadena = (String)it.next();
+				String[] tokens = cadena.split("%");
+				latitud1 = Double.parseDouble(tokens[0]);
+				longitud1 = Double.parseDouble(tokens[1]);
+				//Segunda coordenada
+				if (it.hasNext()) {
+					//Si no hay siguiente es que no hay coordenada.
+					String cadena2 = (String)it.next();
+					String[] tokens2 = cadena2.split("%");
+					latitud2 = Double.parseDouble(tokens2[0]);
+					longitud2 = Double.parseDouble(tokens2[1]);
+					distancia = distancia + distancia(latitud1,longitud1,latitud2,longitud2);
+				}
+			}
+		}
+		
+		//Construimos el mensaje de informe.
+		DecimalFormat df = new DecimalFormat("0.000");
+		String mensaje = "Tiempo: " + horas + " horas " + minutos + " minutos " +
+		segundos + " segundos " + System.getProperty("line.separator");
+		mensaje+="Distancia (km): " + df.format(distancia) + System.getProperty("line.separator");
+		
+		//Calculamos la velocidad media en kilometros hora.
+		double metrossegundo = distancia * 1000 / segundosTotales;
+		double kilometroshora = metrossegundo * (3600/1000);
+		mensaje+="Velocidad Media (km/h): " + df.format(kilometroshora) + System.getProperty("line.separator");
+		
+		//VentanaAlerta.mostrarAlerta(this,mensaje);
+		VentanaAlerta.mostrarDialogo(this, mensaje);
+	}
+	
+	private double distancia(double La1, double Lo1, double La2, double Lo2) {
+        double Distancia = 0, Distancia1 = 0;
+        double r = 6378;
+
+        //Punto inicial
+        La1 = La1* Math.PI / 180;
+        Lo1 = Lo1 * Math.PI / 180;
+
+        // Punto final
+        La2 = La2 * Math.PI / 180;
+        Lo2 = Lo2 * Math.PI / 180;        
+        //Distancia = 6378*Math.acos(Math.sin(La1)*Math.sin(La2)+ Math.cos(La1)* Math.cos(La2)*Math.cos(Lo1 - Lo2));
+        Distancia1 = Math.pow(Math.sin((Lo2 - Lo1) / 2), 2);
+        Distancia1 = Math.cos(La1) * Math.cos(La2) * Distancia1;
+        Distancia1 = Math.pow(Math.sin((La2 - La1) / 2), 2) + Distancia1;
+        Distancia1 = Math.sqrt(Distancia1);
+        Distancia1 = 2 * r * Math.asin(Distancia1);
+        return Distancia1;
+  }
+	
+	public long cantidadTotalSegundos(Calendar fechaInicial ,Calendar fechaFinal) {  
+			long totalSegundos=0; 
+		
+			totalSegundos=((fechaFinal.getTimeInMillis()-fechaInicial.getTimeInMillis())/1000); 
+			return totalSegundos;
+		
+	}
+	    
+}
---
+++ CursoAndroid/src/org/android/cursoandroid/gps/VentanaAlerta.java
@@ -0,0 +1,31 @@
+package org.android.cursoandroid.gps;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.widget.Toast;
+
+public class VentanaAlerta {
+
+	public static void mostrarAlerta(Context ctx, String mensaje) {
+		Toast toast = Toast.makeText(ctx,mensaje, Toast.LENGTH_SHORT); 
+		toast.show();
+	}
+	
+	public static void mostrarDialogo (Context ctx, String mensaje) {
+	
+		AlertDialog.Builder builder = new AlertDialog.Builder(ctx);
+
+		builder.setMessage(mensaje)
+			.setTitle("Informe")
+			.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+               dialog.cancel();
+			}
+		});
+
+		AlertDialog dialog = builder.create();
+		dialog.show();
+		
+	}
+}
